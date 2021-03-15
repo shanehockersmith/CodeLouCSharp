08f6175130fbed4457a2c09d0a4168953586b557
Shane Hockersmith <shane.hockersmith@gmail.com>
2021-03-15 19:20:24 -0400

    GIT TEST

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -7,6 +7,7 @@ namespace FinalProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("GIT TEST!");
         }
     }
 }
